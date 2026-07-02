d9e60bd328cec57cc894024f7282c9edf76d7ac7
Chirag Prajapati <chiragmp1993@gmail.com>
2026-07-02 23:04:45 +0530

    Fix production CORS

--- server.js
+++ server.js
@@ -1,111 +1,50 @@
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
-
-const {Server} = require("socket.io");
-
+const { Server } = require("socket.io");
 
 const app = express();
 
-
-// app.use(cors());
-
-
-const server = http.createServer(app);
-
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://socket-frontend-alpha.vercel.app/",
-  "https://your-frontend.vercel.app",
+  "https://socket-frontend-alpha.vercel.app"
 ];
 
 app.use(
   cors({
     origin: allowedOrigins,
+    credentials: true
   })
 );
+
+const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
     origin: allowedOrigins,
     methods: ["GET", "POST"],
-  },
+    credentials: true
+  }
 });
 
-
-
-// normal api
-
-app.get("/",(req,res)=>{
-
-    res.send("Socket Backend Running");
-
+app.get("/", (req, res) => {
+  res.send("Socket Backend Running");
 });
 
+io.on("connection", (socket) => {
+  console.log("Connected:", socket.id);
 
+  socket.on("message", (data) => {
+    io.emit("message", data);
+  });
 
-
-
-// socket connection
-
-io.on("connection",(socket)=>{
-
-
-    console.log(
-        "New user connected:",
-        socket.id
-    );
-
-
-
-    // receive message from frontend
-
-    socket.on("message",(data)=>{
-
-
-        console.log(
-            "Message :",
-            data
-        );
-
-
-        // send message to all connected users
-
-        io.emit(
-            "message",
-            data
-        );
-
-
-    });
-
-
-
-
-    socket.on("disconnect",()=>{
-
-
-        console.log(
-            "User disconnected",
-            socket.id
-        );
-
-
-    });
-
-
+  socket.on("disconnect", () => {
+    console.log("Disconnected:", socket.id);
+  });
 });
 
+const PORT = process.env.PORT || 5000;
 
-
-
-
-server.listen(5000,()=>{
-
-
-    console.log(
-        "Server running on 5000"
-    );
-
-
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
